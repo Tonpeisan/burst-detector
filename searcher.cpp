52fdd820ca34dcbd318869aec151bf1ddd9228cb
eight <eightmono13@gmail.com>
2019-12-21 04:36:31 +0900

    add gameset detect

--- searcher.cpp
+++ searcher.cpp
@@ -16,6 +16,8 @@ void search_edit_points(string src, deque<double> *edit_points)
     Mat icon = imread("icon.png");
     resize(icon, icon, Size(20, 20));
 
+    Mat gameset_image = imread("gemeset_template.png");
+
     int number_of_frame = 0;
     int last_detected = -framerate * 3;
 
@@ -24,6 +26,7 @@ void search_edit_points(string src, deque<double> *edit_points)
     {
         resize(frame, resized_frame, Size(640, 360));
         Mat cropped_frame(resized_frame, Rect(75, 170, 500, 30));
+
         Mat matching_result;
         matchTemplate(cropped_frame, icon, matching_result, TM_CCOEFF_NORMED);
 
@@ -35,6 +38,20 @@ void search_edit_points(string src, deque<double> *edit_points)
             double timestamp = number_of_frame / framerate;
             edit_points->push_back(timestamp);
             cout << "INFO:BURST DETECTED AT " << timestamp << " sec." << endl;
+            last_detected = number_of_frame;
+        }
+
+        // detect gameset
+        Mat cropped_frame_for_gameset(resized_frame, Rect(116, 44, 414, 204));
+        matchTemplate(cropped_frame_for_gameset, gameset_image, matching_result, TM_CCOEFF_NORMED);
+        match_mask = Mat::zeros(matching_result.size(), CV_8UC1);
+        match_mask.setTo(1, matching_result > 0.60);
+        stock = sum(match_mask)[0];
+        if (stock > 0 && (number_of_frame - last_detected > framerate * 3))
+        {
+            double timestamp = number_of_frame / framerate;
+            edit_points->push_back(timestamp);
+            cout << "INFO:GAME SET IS DETECTED AT " << timestamp << " sec." << endl;
             last_detected = number_of_frame;
         }
         ++number_of_frame;
