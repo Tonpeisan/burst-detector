32d841c39d908dd79a411a57d18f2829a8027812
eight <eightmono13@gmail.com>
2020-01-07 19:59:27 +0900

    GAME SETシーンの検出処理の追加 (#2)
    * add gameset detect
    
    * Fix: 指定したファイター以外の試合のGAME SETは検出しないように修正。
    
    * fix: 通常の撃墜が一回でも検出されていない場合はGAME SETの検出処理を行わないよう修正

--- searcher.cpp
+++ searcher.cpp
@@ -15,15 +15,19 @@ void search_edit_points(string src, deque<double> *edit_points)
 
     Mat icon = imread("icon.png");
     resize(icon, icon, Size(20, 20));
+    Mat gameset_image = imread("gemeset_template.png");
 
     int number_of_frame = 0;
     int last_detected = -framerate * 3;
+
+    bool is_burst_detected = false;
 
     cout << "INFO:SEARCH START. It may take time." << endl;
     while (cap.read(frame))
     {
         resize(frame, resized_frame, Size(640, 360));
         Mat cropped_frame(resized_frame, Rect(75, 170, 500, 30));
+
         Mat matching_result;
         matchTemplate(cropped_frame, icon, matching_result, TM_CCOEFF_NORMED);
 
@@ -36,6 +40,25 @@ void search_edit_points(string src, deque<double> *edit_points)
             edit_points->push_back(timestamp);
             cout << "INFO:BURST DETECTED AT " << timestamp << " sec." << endl;
             last_detected = number_of_frame;
+            is_burst_detected = true;
+        }
+
+        // detect gameset
+        if(is_burst_detected)
+        {
+            Mat cropped_frame_for_gameset(resized_frame, Rect(116, 44, 414, 204));
+            matchTemplate(cropped_frame_for_gameset, gameset_image, matching_result, TM_CCOEFF_NORMED);
+            match_mask = Mat::zeros(matching_result.size(), CV_8UC1);
+            match_mask.setTo(1, matching_result > 0.60);
+            stock = sum(match_mask)[0];
+            if (stock > 0 && (number_of_frame - last_detected > framerate * 3))
+            {
+                double timestamp = number_of_frame / framerate;
+                edit_points->push_back(timestamp);
+                cout << "INFO:GAME SET IS DETECTED AT " << timestamp << " sec." << endl;
+                last_detected = number_of_frame;
+                is_burst_detected = false;
+            }
         }
         ++number_of_frame;
     }
